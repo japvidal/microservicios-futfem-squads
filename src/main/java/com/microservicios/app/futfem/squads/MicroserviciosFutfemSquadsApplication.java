17b618238717b697faf8c5f9c5623ed0f9fba603
japvidal <japvidal@gmail.com>
2026-03-22 18:52:21 +0100

    Initial backend import

---
+++ src/main/java/com/microservicios/app/futfem/squads/MicroserviciosFutfemSquadsApplication.java
@@ -0,0 +1,13 @@
+package com.microservicios.app.futfem.squads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviciosFutfemSquadsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviciosFutfemSquadsApplication.class, args);
+	}
+
+}
